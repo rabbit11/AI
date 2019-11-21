1cf15874c35673957ad5af53f0ca192c2ebbd16c
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-11-21 10:24:07 -0300

    added rand

--- algorithms/single-linkage/single-linkage.py
+++ algorithms/single-linkage/single-linkage.py
@@ -7,22 +7,33 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics.cluster import adjusted_rand_score
 
 print("Escolha o arquivo 1, 2 ou 3 de entrada")
+
 val = int(input())
+
+readClu = []
+resultClu = []
 
 if val == 1:
     c = pd.read_csv("../../datasets/datasets/c2ds1-2sp.csv", sep='\t')
+    readClu = open("../../datasets/datasets/c2ds1-2spReal.clu", 'r')
     kmin = 2
     kmax = 5
 elif val == 2:
     c = pd.read_csv("../../datasets/datasets/c2ds3-2g.csv", sep='\t')
+    readClu = open("../../datasets/datasets/c2ds3-2gReal.clu", 'r')
     kmin = 2
     kmax = 5
 else:
     c = pd.read_csv("../../datasets/datasets/monkey.csv", sep='\t')
+    readClu = open("../../datasets/datasets/monkeyReal1.clu", 'r')
     kmin = 5
     kmax = 12
 
 X = c.iloc[:, [1, 2]].values
+
+for linha in readClu:
+    novaLinha = linha.strip("\n").split("\t")
+    resultClu.append(int(novaLinha[1]))
 
 for x in range(kmin, kmax + 1):
 
@@ -37,16 +48,18 @@ for x in range(kmin, kmax + 1):
     if val == 1:
         title = 'c2ds1-2sp ' + str(x) + 'c'
         plt.title(title)
-        plt.savefig(title)
+        # plt.savefig(title)
     elif val == 2:
         title = 'c2ds3-2g ' + str(x) + 'c'
         plt.title(title)
-        plt.savefig(title)
+        # plt.savefig(title)
     else:
         title = 'monkey ' + str(x) + 'c'
         plt.title(title)
-        plt.savefig(title)
+        # plt.savefig(title)
 
-    # print(adjusted_rand_score(X, ))
+    # print(data)
+    # print(resultClu)
+    print(adjusted_rand_score(data, resultClu))
 
     plt.show()
