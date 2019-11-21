0863b503297ee970dbe91de1d09c3ac30ae1e789
Natham <nathamcoracini@gmail.com>
2019-11-21 12:19:25 -0300

    Conjunto de dados adicionado. AR no average link funcionando

--- algorithms/average-link/average-link.py
+++ algorithms/average-link/average-link.py
@@ -9,18 +9,31 @@ from sklearn.metrics.cluster import adjusted_rand_score
 print("Escolha o arquivo 1, 2 ou 3 de entrada")
 val = int(input())
 
+readClu = []
+resultClu = []
+
 if val == 1:
     c = pd.read_csv("../../datasets/datasets/c2ds1-2sp.csv", sep='\t')
+    readClu = open("../../datasets/datasets/c2ds1-2spReal.clu", 'r')
     kmin = 2;
     kmax = 5;
+    name = 'c2ds1-2sp'
 elif val == 2:
     c = pd.read_csv("../../datasets/datasets/c2ds3-2g.csv", sep='\t')
+    readClu = open("../../datasets/datasets/c2ds3-2gReal.clu", 'r')
     kmin = 2;
     kmax = 5;
+    name = 'c2ds3-2g'
 else:
     c = pd.read_csv("../../datasets/datasets/monkey.csv", sep='\t')
+    readClu = open("../../datasets/datasets/monkeyReal1.clu", 'r')
     kmin = 5;
     kmax = 12;
+    name = 'monkey'
+
+for linha in readClu:
+    novaLinha = linha.strip("\n").split("\t")
+    resultClu.append(int(novaLinha[1]))
 
 X = c.iloc[:, [1, 2]].values
 
@@ -45,8 +58,6 @@ for x in range(kmin, kmax + 1):
         plt.title(title)
         plt.savefig(title)
 
-    # print(adjusted_rand_score(X, ))
-
-
+    print(adjusted_rand_score(data, resultClu))
 
     plt.show()
